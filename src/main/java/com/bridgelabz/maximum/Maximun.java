18398acc843d7f133b8a2a78d610dd239ec001e5
akshay <akshay.yamgar29@gmail.com>
2021-10-08 11:54:52 +0530

    Added: Program to take multiple elements and find max & print

--- src/main/java/com/bridgelabz/maximum/Maximun.java
+++ src/main/java/com/bridgelabz/maximum/Maximun.java
@@ -1,31 +1,23 @@
 package com.bridgelabz.maximum;
 
 public class Maximun<E extends Comparable> {
-    E value1, value2, value3;
 
-    public Maximun(E value1, E value2, E value3) {
-        this.value1 = value1;
-        this.value2 = value2;
-        this.value3 = value3;
-    }
-
-    public E testMaximum(E value1, E value2, E value3) {
-        E testMax = testMaximum(this.value1, this.value2, this.value3);
-        return testMax;
-    }
-
-    public static <E extends Comparable> E getmaximum(E value1, E value2, E value3) {
-        E max = value1;
-        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
-            max = value2;
-        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
-            max = value3;
-        System.out.println(max);
+    public static <E extends Comparable> E testMaximum(E... values) {
+        E max = values[0];
+        for (E element : values) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        toPrint(max, values);
         return max;
     }
 
-    private static <E> void toPrint(E value1, E value2, E value3, E max) {
-        System.out.println("Maximum of "+value1+" , "+value2+" , "+value3+" is: " +max);
+    private static <E> void toPrint(E max, E... values) {
+        for (E element : values) {
+            System.out.println("\t" + element);
+        }
+        System.out.println("max. is " + max);
     }
 }
 
